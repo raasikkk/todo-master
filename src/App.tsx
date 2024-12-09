5a43b2978b49843b844a586886eb58cfb552e234
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-12-09 10:41:31 +0500

    added type animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import Counter from "./components/Counter";
 import Navbar from "./components/Navbar";
 import Todo from "./components/Todo";
+import { ReactTyped } from "react-typed";
 import "./style.scss";
 
 function App() {
@@ -8,9 +9,17 @@ function App() {
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-5xl mt-5 text-center font-extrabold text-violet-700">
-          Todo App with Redux
+        <h1 className="text-5xl  mt-5 text-center font-extrabold text-violet-700">
+          <ReactTyped
+            strings={[`Todo App with Redux`]}
+            typeSpeed={80}
+            backSpeed={60}
+            backDelay={2500}
+            showCursor={true}
+            loop={false}
+          />
         </h1>
+
         <Todo />
       </div>
     </>
